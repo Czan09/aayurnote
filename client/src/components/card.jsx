7ace634654d19e5fdcf6065368caa26de6a8b59a
legendofzer0 <98453046+legendofzer0@users.noreply.github.com>
2024-03-21 12:11:08 +0545

    added delete funtion to notes

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../css/card.css";
+import axios from "axios";
 const Card = ({ card }) => {
   const [id, setId] = useState("");
   const [title, setTitle] = useState("");
@@ -14,18 +15,57 @@ const Card = ({ card }) => {
     setId(card.note_id);
   }, [card]);
 
+  const addTag = () => {
+    console.log(id);
+  };
+  const DeleteNote = async () => {
+    console.log(id);
+    try {
+      const deleteNote = await axios.delete("/api/notes/" + id);
+      console.log(deleteNote);
+      window.location.reload(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   console.log(card);
 
   return (
-    <Link className="cardLink" to={"/note/" + id}>
-      {console.log(id)}
+    <>
+      {/* {console.log(id)} */}
       <div className="card">
-        <div className=""></div>
-        <h3 className="">{title}</h3>
-        <span className="">{tag}</span>
-        <div className="">{content}</div>
+        <table>
+          <tr>
+            <th className="colmn1">
+              <Link className="cardLink" to={"/note/" + id}>
+                <div>
+                  <div className=""></div>
+                  <h3 className="">{title}</h3>
+                  <span className="">{tag}</span>
+                  <div className="">{content}</div>
+                </div>
+              </Link>
+            </th>
+
+            <th className="colmn2">
+              <div>
+                <span>
+                  <button className="button1" onClick={addTag}>
+                    CHANGE TAG
+                  </button>
+                </span>
+                <span>
+                  <button className="button2" onClick={DeleteNote}>
+                    DELETE
+                  </button>
+                </span>
+              </div>
+            </th>
+          </tr>
+        </table>
       </div>
-    </Link>
+    </>
   );
 };
 
