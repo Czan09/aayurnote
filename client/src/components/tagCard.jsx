c31ee4b55566307467fabfa284693351d419c3fe
legendofzer0 <98453046+legendofzer0@users.noreply.github.com>
2024-03-22 11:36:53 +0545

    yes yes finally done user side

--- client/src/components/tagCard.jsx
+++ client/src/components/tagCard.jsx
@@ -4,11 +4,13 @@ import "../css/card.css";
 import axios from "axios";
 const TagCard = ({ card }) => {
   const [id, setId] = useState("");
+  const [color, setColor] = useState();
   const [tag, setTag] = useState("");
 
   useEffect(() => {
     setTag(card.tag_name);
     setId(card.id);
+    setColor(card.color);
   }, [card]);
 
   const deleteTag = async () => {
@@ -32,6 +34,14 @@ const TagCard = ({ card }) => {
             <th className="colmn1">
               <Link className="cardLink" to={"/tag/" + id}>
                 <div>
+                  <div
+                    className="colorBox"
+                    style={{
+                      backgroundColor: color,
+                      width: "50%",
+                      height: "20px",
+                    }}
+                  ></div>
                   <span className="">{tag}</span>
                 </div>
               </Link>
